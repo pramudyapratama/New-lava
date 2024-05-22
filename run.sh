0014374253e5cef1ae58a4f66dfea781ef021baf
Pramudya Pratama <35626595+pramudyapratama@users.noreply.github.com>
2024-05-22 16:50:48 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://RPCETHLAVA/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-046e3b31-f121-408b-8f49-b2bbb58db7f8/)
 
 
 echo "$curl_output" >> Update.tsx
